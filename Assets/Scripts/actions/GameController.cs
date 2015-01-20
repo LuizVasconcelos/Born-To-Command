4d4ccef6475dcd4063ae78eba5129e0ddba27ff6
Luana Martins <lms7@cin.ufpe.br>
2015-01-20 20:43:41 -0300

    Save/Load Game in XML
    Classe GameController.cs

--- Assets/Scripts/actions/GameController.cs
+++ Assets/Scripts/actions/GameController.cs
@@ -1,17 +1,58 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
 using System.Collections;
+using System.IO;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
+	
+	/**
+	http://stackoverflow.com/questions/17286308/how-to-write-an-xml-file-in-c-sharp-in-unity
 
-	// TODO XML files GameController to save/load game status
-
-	public void SaveGame(){
-		// TODO Saving game status
-	}	
-
-	public void LoadGame(){
-		// TODO Loading game status
+	 */
+	
+	void Awake(){
+		/*
+		XmlSerializer xmls = new XmlSerializer(typeof(Player));
+		
+		StringWriter sw = new StringWriter();
+		xmls.Serialize(sw, new Player { Level = 5, Health = 500 });
+		string xml = sw.ToString();
+		
+		Player player = xmls.Deserialize(new StringReader(xml)) as Player;
+		Debug.Log (xml);
+		*/
+		
+		XmlSerializer xmls = new XmlSerializer(typeof(Player));
+		string path = Application.persistentDataPath + "/PlayerData.xml";
+		using (var stream = File.OpenWrite(path)){
+			xmls.Serialize(stream, new Player { Level = 5, Health = 500, Lista = new List<string>{"string1", "string2"} });
+		}
+		Debug.Log (path);
+		
+		// Reading data from Player
+		Player player = null;
+		using (var stream = File.OpenRead(path)){
+			player = xmls.Deserialize(stream) as Player;
+		}
+		
+		Debug.Log ("Health: " + player.Health + " Level: " + player.Level);
 	}
+}
+
+[XmlRoot]
+public class Player
+{
+	[XmlElement]
+	public int Level { get; set; }
+	
+	[XmlElement]
+	public int Health { get; set; }
+	
+	[XmlArray("Lista")]
+	[XmlArrayItem("string")]
+	public List<string> Lista;
 }
 

f5885e2c545345031c9d08362ee0b83dbb62f279
Luiz Filho <lavf@cin.ufpe.br>
2015-01-20 23:23:50 -0200

    Main Scene Buttons + Animation
    Inserção dos outros botões do menu referentes às possíveis ações do
    jogador quando não estiver em missão/batalha

--- Assets/Scripts/scriptMainScene.cs
+++ Assets/Scripts/scriptMainScene.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class scriptMainScene : MonoBehaviour {
 
 	private bool isBlacksmithClicked;
+	private bool isMedicalCenterClicked;
+	private bool isTrainingCenterClicked;
+	private bool isCarpenterClicked;
 
 	// Use this for initialization
 	void Start () {
 		isBlacksmithClicked = false;
+		isMedicalCenterClicked = false;
+		isTrainingCenterClicked = false;
+		isCarpenterClicked = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(isBlacksmithClicked){
+		if (isBlacksmithClicked) 
+		{
 			// current camera depth
 			float current = Camera.main.camera.orthographicSize;
 			// hardcoded target depth
@@ -23,16 +30,93 @@ public class scriptMainScene : MonoBehaviour {
 			float smoothTime = 0.15f;
 			
 			// zoom phase
-			Camera.main.camera.orthographicSize = Mathf.SmoothDamp(current, target, ref currentVelocity, smoothTime);
+			Camera.main.camera.orthographicSize = Mathf.SmoothDamp (current, target, ref currentVelocity, smoothTime);
 
 			// open phase
-			if(Camera.main.camera.orthographicSize <= 0.06f){
-				Application.LoadLevel("blacksmithScene");
+			if (Camera.main.camera.orthographicSize <= 0.06f) 
+			{
+				Application.LoadLevel ("blacksmithScene");
+			}
+		} 
+		else if (isMedicalCenterClicked) 
+		{
+			// current camera depth
+			float current = Camera.main.camera.orthographicSize;
+			// hardcoded target depth
+			float target = 0.05f;
+			// 0.0 for default
+			float currentVelocity = 0.0f;
+			// 0.15 for default
+			float smoothTime = 0.15f;
+			
+			// zoom phase
+			Camera.main.camera.orthographicSize = Mathf.SmoothDamp (current, target, ref currentVelocity, smoothTime);
+			
+			// open phase
+			if (Camera.main.camera.orthographicSize <= 0.06f) 
+			{
+				Application.LoadLevel ("medicalCenterScene");
+			}
+		}
+		else if (isTrainingCenterClicked) 
+		{
+			// current camera depth
+			float current = Camera.main.camera.orthographicSize;
+			// hardcoded target depth
+			float target = 0.05f;
+			// 0.0 for default
+			float currentVelocity = 0.0f;
+			// 0.15 for default
+			float smoothTime = 0.15f;
+			
+			// zoom phase
+			Camera.main.camera.orthographicSize = Mathf.SmoothDamp (current, target, ref currentVelocity, smoothTime);
+			
+			// open phase
+			if (Camera.main.camera.orthographicSize <= 0.06f) 
+			{
+				Application.LoadLevel ("trainingCenterScene");
+			}
+		}
+		else if (isCarpenterClicked) 
+		{
+			// current camera depth
+			float current = Camera.main.camera.orthographicSize;
+			// hardcoded target depth
+			float target = 0.05f;
+			// 0.0 for default
+			float currentVelocity = 0.0f;
+			// 0.15 for default
+			float smoothTime = 0.15f;
+			
+			// zoom phase
+			Camera.main.camera.orthographicSize = Mathf.SmoothDamp (current, target, ref currentVelocity, smoothTime);
+			
+			// open phase
+			if (Camera.main.camera.orthographicSize <= 0.06f) 
+			{
+				Application.LoadLevel ("carpenterScene");
 			}
 		}
 	}
 
-	void onBlacksmithClicked(){
+	void onBlacksmithClicked()
+	{
 		isBlacksmithClicked = true;
+	}
+
+	void onMedicalCenterClicked()
+	{
+		isMedicalCenterClicked = true;
+	}
+
+	void onTrainingCenterClicked()
+	{
+		isTrainingCenterClicked = true;
+	}
+	
+	void onCarpenterClicked()
+	{
+		isCarpenterClicked = true;
 	}
 }

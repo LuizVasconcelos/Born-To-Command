ef253eafdf253e12552f29820ca1dc60622b20b1
Luiz Filho <lavf@cin.ufpe.br>
2015-01-21 19:04:03 -0200

    Buttons animations modification
    Título autoexplicativo...

---
+++ Assets/Scripts/scriptCarpenterScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptCarpenterScene : MonoBehaviour {
+
+	private float current;
+	private float target;
+	private float currentVelocity;
+	private float smoothTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = 1.1f;
+		currentVelocity = 0.0f;
+		smoothTime = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Camera.main.camera.orthographicSize < target) 
+		{
+			
+			// current camera depth
+			current = Camera.main.camera.orthographicSize;
+			
+			// zoom phase
+			Camera.main.camera.orthographicSize = Mathf.SmoothDamp (current, target, ref currentVelocity, smoothTime);
+			
+		}
+	}
+}

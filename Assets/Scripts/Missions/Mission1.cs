ed705130e032c9eaf97a31eb8ed9f6564cad11d0
Luana Martins <lms7@cin.ufpe.br>
2015-01-23 21:32:45 -0300

    Save/Load Game in XML [3]

---
+++ Assets/Scripts/Missions/Mission1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mission1 {
+
+	// Recompensas
+	public float gold; // Quantidade de Ouro
+	public int herd; // Cabeças de gado
+	// 
+
+	void StartMission1(){
+		// TODO Implementation of Mission 1
+	}
+
+	void ProcessFarm(){
+		// TODO Farm
+		// Passar ou nao pela fazenda
+		// Incendiar ou nao a fazenda
+	}
+
+	void ProcessCastle(){
+		// TODO Castle
+		int numberOfNortenhos = Random.Range(0, 10);
+
+	}
+
+	void ProcessForest(){
+		// TODO Forest
+		int numberOfNortenhos = Random.Range(0, 10);
+
+	}
+
+	void ProcessMountain(){
+		// TODO Mountain
+
+	}
+
+}

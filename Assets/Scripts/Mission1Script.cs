d3d4e5b0b59d974e52e82187002dfb40969b1223
Victor Pimenta <vfp@cin.ufpe.br>
2015-02-04 14:50:44 -0300

    Finished phase 1
    Completado os efeitos da fase 1 da missão 1.

--- Assets/Scripts/Mission1Script.cs
+++ Assets/Scripts/Mission1Script.cs
@@ -1,30 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine;
+using UnityEditor;
 
 public class Mission1Script : MonoBehaviour {
 
 	private int index;
-	private bool spacePressed;
-
+	private bool village3Clicked;
+	private bool castleClicked;
 	private float startTime;
+
+	// Action buttons
+	private bool isGoClicked;
 
 	// Use this for initialization
 	void Start () {
-		index = GameObject.FindGameObjectsWithTag("Route").Length-1;
-		spacePressed = false;
+		village3Clicked = false;
+		castleClicked = false;
 		startTime = Time.time;
+
+		isGoClicked = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((Input.GetKeyDown (KeyCode.Space) || spacePressed)&&index>=0) {
-			spacePressed = true;
-			show (index);
+
+		/**************************** PHASE 1 ********************************/
+
+		// Lines exposition
+		if (village3Clicked && index >= 0) {
+			if(castleClicked){
+				show (index,2);
+				index--;
+			}else{
+				show (index,1);
+				index--;
+			}
+		}else if(castleClicked && index >= 0){
+			show (index,3);
 			index--;
 		}
-		if(Input.GetMouseButtonDown(0)){
 
+		// Click check
+		if(Input.GetMouseButtonDown(0)){
+			
 			//Debug.Log("Mouse is down");
+			
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			if (hit){
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+				// Clicked nearest village
+				if (hitInfo.transform.gameObject.name == "Village3-obj" && !castleClicked){
+					village3Clicked = true;
+					index = GameObject.FindGameObjectsWithTag("Route-1").Length-1;
+				// Clicked the castle
+				} else if(hitInfo.transform.gameObject.name == "Castle-obj"){
+					castleClicked = true;
+
+					// Indirect route
+					if(village3Clicked){
+						index = GameObject.FindGameObjectsWithTag("Route-2").Length-1;
+					// Direct route
+					}else{
+						index = GameObject.FindGameObjectsWithTag("Route-3").Length-1;
+					}
+				} else {
+					Debug.Log ("TAG: " + hitInfo.transform.gameObject.tag);
+				}
+			} else {
+				Debug.Log("No hit");
+			}
+		}
+		/*********************************************************************/
+
+		/**************************** PHASE 2 ********************************/
+		if(isGoClicked){
 			
 			RaycastHit hitInfo = new RaycastHit();
 			GameObject personagem = GameObject.Find ("Personagem");
@@ -48,13 +99,58 @@ public class Mission1Script : MonoBehaviour {
 				Debug.Log("No hit");
 			}
 		}
+		/*********************************************************************/
 	}
 
-	void show(int idx){
-		GameObject[] labels =  GameObject.FindGameObjectsWithTag("Route");
+	void show(int idx, int route){
+		GameObject[] labels =  GameObject.FindGameObjectsWithTag("Route-"+route);
 
 		Vector3 pos = labels[idx].transform.position;
 		pos.z = 0.0f;
 		labels[idx].transform.position = pos;
+	}
+
+	void OnGoClicked(){
+		if (EditorUtility.DisplayDialog ("March to Battle!", //title
+		                                 "Are you sure you want to follow this route to battle?", // text
+		                                 "Yes", "No")) { // yes, no
+			isGoClicked = true;
+		}
+	}
+
+	void onCancelClicked(){
+		// Clear lines
+		for (int i = 1; i<=3; i++) {
+			GameObject[] labels =  GameObject.FindGameObjectsWithTag("Route-"+i);
+
+			foreach (GameObject label in labels){
+				Vector3 pos = label.transform.position;
+				pos.z = 0.05f;
+				label.transform.position = pos;
+			}
+		}
+
+		// Clear variables
+		village3Clicked = false;
+		castleClicked = false;
+	}
+
+	GameObject FindClosestLabel(int route) {
+		GameObject personagem = GameObject.Find ("Personagem");
+
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("Route-"+route);
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = personagem.transform.position;
+		foreach (GameObject go in gos) {
+			Vector3 diff = go.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closest = go;
+				distance = curDistance;
+			}
+		}
+		return closest;
 	}
 }

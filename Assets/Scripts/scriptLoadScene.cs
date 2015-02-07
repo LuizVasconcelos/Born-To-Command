075233b5fbda5829bdc99346628507d5b9f2a955
Luana Martins <lms7@cin.ufpe.br>
2015-02-07 20:56:57 -0300

    Load Scene and Script
    Script e Unity Scenes para carregar os dados do Game atual.

---
+++ Assets/Scripts/scriptLoadScene.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class scriptLoadScene : MonoBehaviour {
+	private int numberGame = 0;
+	private Player player = new Player(10,10, new Troop(new List<Unit>()), 10, 10);
+	
+	void OnButtonClicked1(){
+		Debug.Log ("Load Game 1");
+		// TODO
+	}
+	
+	void OnButtonClicked2(){
+		Debug.Log ("Load Game 1");
+		// TODO
+	}
+	
+	void OnButtonClicked3(){
+		Debug.Log ("Load Game 1");
+		// TODO
+	}
+	
+	void OnButtonClicked4(){
+		Debug.Log ("Load Game 1");
+		// TODO
+	}
+	
+	void OnButtonStartClicked(){
+		// TODO
+		Debug.Log ("mainScene");
+		Application.LoadLevel("mainScene");
+	}
+}

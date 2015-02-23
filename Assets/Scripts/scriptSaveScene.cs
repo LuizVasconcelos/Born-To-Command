bf6e7c11eff37b2d684b0e6fa4cdabed584e28a4
Victor Pimenta <vfp@cin.ufpe.br>
2015-02-23 16:38:05 -0300

    Changed Save Scene

--- Assets/Scripts/scriptSaveScene.cs
+++ Assets/Scripts/scriptSaveScene.cs
@@ -2,30 +2,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class scriptSaveScene : MonoBehaviour {
 
 	// Game id default
 	private int numberGame = 1; 
 
-	void OnButtonClicked1(){
+	void Start(){
+		for (int i = 1; i < 5; i++) {
+			try{
+				Player p = (new GameController ()).LoadGame (i);
+				GameObject log = GameObject.Find ("Label"+i);
+				UILabel content = log.GetComponent<UILabel> ();
+				content.text = "Mission 1; Gold: "+p.Gold+"; Units: "+p.Units.Units.Count+";";
+			}catch(FileNotFoundException e){
+				GameObject log = GameObject.Find ("Label"+i);
+				UILabel content = log.GetComponent<UILabel> ();
+				content.text = "Empty";
+			}
+		}
+	}
+
+	void Onckb1Activate(){
 		numberGame = 1;
-		Debug.Log ("Button 1 clicked.");
 	}
-
-	void OnButtonClicked2(){
+	
+	void Onckb2Activate(){
 		numberGame = 2;
-		Debug.Log ("Button 2 clicked.");
 	}
-
-	void OnButtonClicked3(){
+	
+	void Onckb3Activate(){
 		numberGame = 3;
-		Debug.Log ("Button 3 clicked.");
 	}
-
-	void OnButtonClicked4(){
+	
+	void Onckb4Activate(){
 		numberGame = 4;
-		Debug.Log ("Button 4 clicked.");
 	}
 
 	void OnButtonSaveClicked(){

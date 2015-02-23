05dfce2dd6e76b0c4df41d8187822c6a2b42ac9d
Victor Pimenta <vfp@cin.ufpe.br>
2015-02-23 16:08:14 -0300

    Changed Load Scene

--- Assets/Scripts/scriptLoadScene.cs
+++ Assets/Scripts/scriptLoadScene.cs
@@ -2,30 +2,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class scriptLoadScene : MonoBehaviour {
 	private int numberGame = 0;
 	private Player player = new Player(10,10, new Troop(new List<Unit>()), 10, 10, new bool[]{false});
+
+	void Start(){
+		for (int i = 1; i < 5; i++) {
+			try{
+				Player p = (new GameController ()).LoadGame (i);
+				GameObject log = GameObject.Find ("Label"+i);
+				UILabel content = log.GetComponent<UILabel> ();
+				content.text = "Mission 1; Gold: "+p.Gold+"; Units: "+p.Units.Units.Count+";";
+			}catch(FileNotFoundException e){
+				GameObject log = GameObject.Find ("Label"+i);
+				UILabel content = log.GetComponent<UILabel> ();
+				content.text = "Empty";
+			}
+		}
+	}
 	
-	void OnButtonClicked1(){
+	void Onckb1Activate(){
 		numberGame = 1;
 		Debug.Log ("Load Game 1");
 		// TODO
 	}
 	
-	void OnButtonClicked2(){
+	void Onckb2Activate(){
 		numberGame = 2;
 		Debug.Log ("Load Game 2");
 		// TODO
 	}
 	
-	void OnButtonClicked3(){
+	void Onckb3Activate(){
 		numberGame = 3;
 		Debug.Log ("Load Game 3");
 		// TODO
 	}
 	
-	void OnButtonClicked4(){
+	void Onckb4Activate(){
 		numberGame = 4;
 		Debug.Log ("Load Game 4");
 		// TODO
@@ -34,6 +50,10 @@ public class scriptLoadScene : MonoBehaviour {
 	void OnButtonStartClicked(){
 		// TODO
 		GameManager.currentNumberGame = numberGame;
+		Player p = (new GameController ()).LoadGame (numberGame);
+		GameManager.player = p;
+
+
 		Debug.Log ("mainScene");
 		Application.LoadLevel("mainScene");
 	}
